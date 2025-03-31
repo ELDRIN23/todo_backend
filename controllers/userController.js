f8cd6d281b1e1e11fdd5dd5073332ea9f5eac97a
eldrin23 <eldrinjohnsom230@gmail.com>
2025-03-31 21:07:16 +0530

    y

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import { Router } from "express";
 import User from "../models/userModel.js";
 
 
@@ -5,34 +6,27 @@ import User from "../models/userModel.js";
 //create a new task
 export const createTask = async(req,res)=>{
    try {
-      // const {task} = req.body;
-      // const newTask = new User({task})
-      // await newTask.save();
-      // res.status(201).json(newTask)
-
       const {task} = req.body;
-     // console.log(req.body)
-
-      if(!task){
+      if(!task.trim()){
          return res.status(400).json({err:"task is required"})
       }
-      const newTask = new User({task})
-
-     // console.log(task);
-      await newTask.save();
-      res.status(201).json(newTask)
+      const newTask = await User.create({
+         task: task.trim()
+      })
+      res.status(201).json({newTask,message:"task added succesfully"})
    } catch (err) {
       res.status(500).json({err:"Internal server Error"})
-      console.log(err)
+
    }
 };
 
 
-//read all task
+//fetch all task
 export const getAllTasks = async(req,res)=>{
    try {
-      await task.find().sort({createdAt: -1 });
-      res.status(201).json(tasks)
+      const tasks = await User.find().sort({createdAt: -1 });
+      //console.log(tasks);
+      res.status(200).json(tasks)
    } catch (err) {
       res.status(500).json({err:"Internal server Error"})
    }
@@ -43,10 +37,33 @@ export const getAllTasks = async(req,res)=>{
 export const updateTask = async(req,res)=>{
    try {
       const {taskId} = req.params; //to get the id
-      const {task} = req.body  //to get the content what to be changed
-      const updatedTask = await task.findByIdAndUpdate(taskId, {task}, {new: true})
+      // console.log(req.params)
 
-      res.status(201).json(updateTask)
+      const {task, status} = req.body  //to get the content what to be changed
+      // console.log(req.body)
+
+      if(!taskId){
+         res.status(404).send({message:"task id not found!"})
+      }
+      const updatedTask = await User.findByIdAndUpdate(taskId, {task}, {new: true})
+      //   1st parameter => taskid which is from the req.params written in the Router
+      //   2nd parameter => {task} here is a key that matches the field name in your schema.
+      //   3rd parameter =>to send the new updated data to change in the DB
+
+      if(!updatedTask){
+         res.status(404).json({message:"task not found"})
+      }
+
+      if(task.trim()){
+         updatedTask.task.trim()
+      }
+      
+      if(status.trim()){
+         updatedTask.status.trim()
+      }
+      updatedTask.save()
+
+      res.status(201).json({updatedTask,message:"task updated succesfully"})
    } catch (err) {
       res.status(500).json({err:"Internal server Error"})
    }
@@ -54,11 +71,27 @@ export const updateTask = async(req,res)=>{
 
 
 //delete task
-export const deleteTask = async(res,req)=>{
+export const deleteTask = async(req,res)=>{
    try {
       const {taskId} = req.params;
-      await task.findByIdAndDelete(taskId);
+      //console.log(taskId)
+
+      if(!taskId){
+        return res.status(404).json({message:"task id not found"})
+      }
+
+      const deletedTask = await User.findByIdAndDelete(taskId);
+
+      if(!deletedTask){
+       return  res.status(400).json({message:"task not found || already deleted" })
+      }
+
+      res.status(201).json({message:"task deleted succesfully"})
    } catch (err) {
       res.status(500).json({err:"Internal server Error"})
    }
 };
+
+
+
+//eldrinTodo
--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,18 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    task:{type:String},
-    createdAt: { type: Date, default: Date.now },
+    task: {
+        type:String,
+        required: true 
+    },
+    createdAt: {
+         type: Date,
+         default: Date.now 
+    },
+    status: {
+        type:String,
+        enum: ["pending","completed"],
+        default: "pending"
+    }
 });
 export default mongoose.model("User", userSchema);
